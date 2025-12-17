398034433ecc4008196da1ba42090f644600c809
phamhaan1987 <phamhaan1987@gmail.com>
2025-12-17 15:17:07 +0700

    Changed chapter3.R

--- chapter3.R
+++ chapter3.R
@@ -1,1 +1,13 @@
-class(2)
+class(2)    # numeric
+typeof(2)   # double
+mode(2)     # numeric
+storage.mode(2) # double
+
+class(3L)   # integer
+class("A")  # character
+class("Hello, world")   # character
+class(2 + 3i)   # complex
+class(-2:2) # integer
+class(0.2:4.5)  # numeric
+class(3 > 2)    # logical
+class(c(TRUE, FALSE))   # logical
